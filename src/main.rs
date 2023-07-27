44ab97652aeca41e317755a4ea483cd03a9735d2
gihrig <glen@arkadias.com>
2023-07-27 00:29:37 -0700

    leptos tut(lesson): 03.4b Dynamic Rendering with <For/>

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,105 @@
-/** 3.4a Iteration - Static Views with Vec<_> */
+/** 3.4b Iteration - Dynamic Rendering with <For/> */
 use leptos::*;
 
-// Sometimes you need to show an item repeatedly, but the
-// list you’re drawing from does not often change. In this
-// case, it’s important to know that you can insert any
-// Vec<IV> where IV: IntoView into your view. In other
-// words, if you can render T, you can render Vec<T>.
-
-/*
-  let values = vec![0, 1, 2];
-  view! { cx,
-      // this will just render "012"
-      <p>{values.clone()}</p>
-      // or we can wrap them in <li>
-      <ul>
-          {values.into_iter()
-              .map(|n| view! { cx, <li>{n}</li>})
-              .collect::<Vec<_>>()}
-      </ul>
-  }
-*/
-
-// Leptos also provides a .collect_view(cx) helper function
-// that allows you to collect any iterator of T: IntoView
-// into Vec<View>.
-
-/*
-  let values = vec![0, 1, 2];
-  view! { cx,
-      // this will just render "012"
-      <p>{values.clone()}</p>
-      // or we can wrap them in <li>
-      <ul>
-          {values.into_iter()
-              .map(|n| view! { cx, <li>{n}</li>})
-              .collect_view(cx)}
-      </ul>
-}
-*/
-
-// The fact that the list is static doesn’t mean the interface
-// needs to be static. You can render dynamic items as part of
-// a static list.
-
-/*
-  // create a list of N signals
-  let counters = (1..=length).map(|idx| create_signal(cx, idx));
-
-  // each item manages a reactive view
-  // but the list itself will never change
-  let counter_buttons = counters
-      .map(|(count, set_count)| {
-          view! { cx,
-              <li>
-                  <button
-                      on:click=move |_| set_count.update(|n| *n += 1)
-                  >
-                      {count}
-                  </button>
-              </li>
-          }
-      })
-      .collect_view(cx);
-
-  view! { cx,
-      <ul>{counter_buttons}</ul>
-  }
-*/
-
-// You can render a Fn() -> Vec<_> reactively as well. But
-// note that every time it changes, this will re-render every
-// item in the list. This is quite inefficient!
-
-// This example will show you a method for mostly-static lists,
-// using Rust iterators
+// This example will show you a method for lists that grow,
+// shrink, or move items, using <For/>
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
     view! { cx,
         <h1>"Iteration"</h1>
-        <h2>"Static List"</h2>
-        <p>"Use this pattern if the list itself is static."</p>
-        <StaticList length=5/>
+        <h2>"Dynamic List"</h2>
+        <p>"Use this pattern if the rows in your list will change."</p>
+        <DynamicList initial_length=5/>
     }
 }
 
-/// A list of counters, without the ability
-/// to add or remove any.
+/// A list of counters that allows you to add or
+/// remove counters.
 #[component]
-fn StaticList(
+fn DynamicList(
     cx: Scope,
-    /// How many counters to include in this list.
-    length: usize,
+    /// The number of counters to begin with.
+    initial_length: usize,
 ) -> impl IntoView {
-    // create counter signals that start at incrementing numbers
-    let counters = (1..=length).map(|idx| create_signal(cx, idx));
+    // This dynamic list will use the <For/> component.
+    // <For/> is a keyed list. This means that each row
+    // has a defined key. If the key does not change, the row
+    // will not be re-rendered. When the list changes, only
+    // the minimum number of changes will be made to the DOM.
 
-    // when you have a list that doesn't change, you can
-    // manipulate it using ordinary Rust iterators
-    // and collect it into a Vec<_> to insert it into the DOM
-    let counter_buttons = counters
-        .map(|(count, set_count)| {
-            view! { cx,
-                <li>
-                    <button
-                        on:click=move |_| set_count.update(|n| *n += 1)
-                    >
-                        {count}
-                    </button>
-                </li>
-            }
-        })
+    // `next_counter_id` will let us generate unique IDs
+    // we do this by simply incrementing the ID by one
+    // each time we create a counter
+    let mut next_counter_id = initial_length;
+
+    // we generate an initial list as in <StaticList/> (commit 3.4a)
+    // but this time we include the ID along with the signal
+    let initial_counters = (0..initial_length)
+        .map(|id| (id, create_signal(cx, id + 1)))
         .collect::<Vec<_>>();
 
-    // Note that if `counter_buttons` were a reactive list
-    // and its value changed, this would be very inefficient:
-    // it would rerender every row every time the list changed.
+    // now we store that initial list in a signal
+    // this way, we'll be able to modify the list over time,
+    // adding and removing counters, and it will change reactively
+    let (counters, set_counters) = create_signal(cx, initial_counters);
+
+    let add_counter = move |_| {
+        // create a signal for the new counter
+        let sig = create_signal(cx, next_counter_id + 1);
+        // add this counter to the list of counters
+        set_counters.update(move |counters| {
+            // since `.update()` gives us `&mut T`
+            // we can just use normal Vec methods like `push`
+            counters.push((next_counter_id, sig))
+        });
+        // increment the ID so it's always unique
+        next_counter_id += 1;
+    };
+
     view! { cx,
-        <ul>{counter_buttons}</ul>
+        <div>
+            <button on:click=add_counter>
+                "Add Counter"
+            </button>
+            <ul>
+                // The <For/> component is central here
+                // This allows for efficient, key list rendering
+                <For
+                    // `each` takes any function that returns an iterator
+                    // this should usually be a signal or derived signal
+                    // if it's not reactive, just render a Vec<_> instead of <For/>
+                    each=counters
+                    // the key should be unique and stable for each row,
+                    // using an index is usually a bad idea, unless your list
+                    // can only grow, because moving items around inside the list
+                    // means their indices will change and they will all rerender
+                    key=|counter| counter.0
+                    // the view function receives each item from your `each` iterator
+                    // and returns a view
+                    view=move |cx, (id, (count, set_count))| {
+                        view! { cx,
+                            <li>
+                                <button
+                                    on:click=move |_| set_count.update(|n| *n += 1)
+                                >
+                                    {count}
+                                </button>
+                                <button
+                                    on:click=move |_| {
+                                        set_counters.update(|counters| {
+                                            counters.retain(|(counter_id, _)| counter_id != &id)
+                                        });
+                                    }
+                                >
+                                    "Remove"
+                                </button>
+                            </li>
+                        }
+                    }
+                />
+            </ul>
+        </div>
     }
 }
 
