9182a4134f4978947aa54df2869efb4df3d28ba0
gihrig <glen@arkadias.com>
2023-07-31 14:13:17 -0700

    leptos tut(lesson): 03.7c Error Handling - Final Example

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,52 @@
-/* 3.7b Error Handling - <ErrorBoundary/> */
+/* 3.7c Error Handling - Final Example */
 
 use leptos::*;
-
-// An <ErrorBoundary/> is a little like the <Show/> component
-// we saw in the last chapter. If everything’s okay—which is
-// to say, if everything is `Ok(_)` it renders its children. But
-// if there’s an `Err(_)` rendered among those children, it will
-// trigger the <ErrorBoundary/>’s fallback.
-
-// Let’s add an <ErrorBoundary/> to this example.
-
 #[component]
-fn NumericInput(cx: Scope) -> impl IntoView {
+fn App(cx: Scope) -> impl IntoView {
     let (value, set_value) = create_signal(cx, Ok(0));
 
-    // When input changes, try to parse a number from the input.
+    // when input changes, try to parse a number from the input
     let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
 
-    view! {cx,
-      <h1>"Error Handling"</h1>
-      <label>
-        "Type a number (or something that's not a number!) "
-        <input on:input=on_input/>
-        <ErrorBoundary
-          // The fallback receives a signal containing an error
-          fallback=|cx, errors| view! { cx,
-            <div class="error">
-              <p>"Not a number! Errors:"</p>
-              // We can render a list of errors as strings, if we'd like
-              <ul>
-                {move || errors.get()
-                  .into_iter()
-                  .map(|(_, e)| view! { cx, <li>{e.to_string()}</li>})
-                  .collect_view(cx)
+    view! { cx,
+        <h1>"Error Handling"</h1>
+        <label>
+            "Type a number (or something that's not a number!) "
+            <input type="number" on:input=on_input/>
+            // If an `Err(_) had been rendered inside the <ErrorBoundary/>,
+            // the fallback will be displayed. Otherwise, the children of the
+            // <ErrorBoundary/> will be displayed.
+            <ErrorBoundary
+                // the fallback receives a signal containing current errors
+                fallback=|cx, errors| view! { cx,
+                    <div class="error">
+                        <p>"Not a number! Errors: "</p>
+                        // we can render a list of errors
+                        // as strings, if we'd like
+                        <ul>
+                            {move || errors.get()
+                                .into_iter()
+                                .map(|(_, e)| view! { cx, <li>{e.to_string()}</li>})
+                                .collect::<Vec<_>>()
+                            }
+                        </ul>
+                    </div>
                 }
-              </ul>
-            </div>
-          }
-        >
-        <p>
-          "You entered " <strong>{value}</strong>
-        </p>
-        </ErrorBoundary>
-      </label>
+            >
+                <p>
+                    "You entered "
+                    // because `value` is `Result<i32, _>`,
+                    // it will render the `i32` if it is `Ok`,
+                    // and render nothing and trigger the error boundary
+                    // if it is `Err`. It's a signal, so this will dynamically
+                    // update when `value` changes
+                    <strong>{value}</strong>
+                </p>
+            </ErrorBoundary>
+        </label>
     }
 }
 
 fn main() {
-    leptos::mount_to_body(|cx| view! { cx, <NumericInput/> })
+    leptos::mount_to_body(|cx| view! { cx, <App/> })
 }
-
-// Now, if you type 42, value is Ok(42) and you’ll see
-
-/*
-  You entered 42
-*/
-
-// If you type "foo", value is Err(_) and the fallback
-// will render. We’ve chosen to render the list of
-// errors as a String, so you’ll see something like
-
-/*
-  Not a number! Errors:
-  - cannot parse integer from empty string
-*/
-
-// If you fix the error, the error message will disappear
-// and the content you’re wrapping in an <ErrorBoundary/>
-// will appear again.
