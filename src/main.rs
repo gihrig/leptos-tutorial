56ee7993ddf24fafa0af10f6950e55421e2890a8
gihrig <glen@arkadias.com>
2023-08-02 01:26:01 -0700

    leptos tut(lesson): 3.9c Component Children - Final Example

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,92 @@
-/* 3.9b Component Children - Manipulating Children */
+/* 3.9c Component Children - Final Example */
 use leptos::*;
 
-// The Fragment type is basically a way of wrapping a Vec<View>. You can
-// insert it anywhere into your view.
-
-// But you can also access those inner views directly to manipulate them.
-// For example, here’s a component that takes its children and turns them
-// into an unordered list.
-
-// ------------------------------------------------------------------
-
-// Wraps Children
+// Often, you want to pass some kind of child view to another
+// component. There are two basic patterns for doing this:
+// - "render props": creating a component prop that takes a function
+//   that creates a view
+// - the `children` prop: a special property that contains content
+//   passed as the children of a component in your view, not as a
+//   property
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
+    let (items, _set_items) = create_signal(cx, vec![0, 1, 2]);
+    let render_prop = move || {
+        // items.with(...) reacts to the value without cloning
+        // by applying a function. Here, we pass the `len` method
+        // on a `Vec<_>` directly
+        let len = move || items.with(Vec::len);
+        view! { cx, <p>"Length: " {len}</p> }
+    };
+
     view! { cx,
-      <h1>"Wraps Children"</h1>
-      <p>"Creates an unordered list of its children"</p>
+      // This component just displays the two kinds of children,
+      // embedding them in some other markup
+      // for component props, you can shorthand
+      <TakesChildren // `render_prop=render_prop` => `render_prop`
+      // (this doesn't work for HTML element attributes)
+      render_prop>
+        // these look just like the children of an HTML element
+        <p>"Here's a child."</p>
+        <p>"Here's another child."</p>
+      </TakesChildren>
+      <hr/>
+      // This component actually iterates over and wraps the children
       <WrapsChildren>
-        "A"
-        "B"
-        "C"
+        <p>"Here's a child."</p>
+        <p>"Here's another child."</p>
       </WrapsChildren>
     }
 }
 
+/// Displays a `render_prop` and some children within markup.
+#[component]
+pub fn TakesChildren<F, IV>(
+    cx: Scope,
+    /// Takes a function (type F) that returns anything that can be
+    /// converted into a View (type IV)
+    render_prop: F,
+    /// `children` takes the `Children` type
+    /// this is an alias for `Box<dyn FnOnce(Scope) -> Fragment>`
+    /// ... aren't you glad we named it `Children` instead?
+    children: Children,
+) -> impl IntoView
+where
+    F: Fn() -> IV,
+    IV: IntoView,
+{
+    view! { cx,
+      <h1>
+        <code>"<TakesChildren/>"</code>
+      </h1>
+      <h2>"Render Prop"</h2>
+      {render_prop()}
+      <hr/>
+      <h2>"Children"</h2>
+      {children(cx)}
+    }
+}
+
+/// Wraps each child in an `<li>` and embeds them in a `<ul>`.
 #[component]
 pub fn WrapsChildren(cx: Scope, children: Children) -> impl IntoView {
-    // Fragment has `nodes` field that contains a Vec<View>
+    // children(cx) returns a `Fragment`, which has a
+    // `nodes` field that contains a Vec<View>
+    // this means we can iterate over the children
+    // to create something new!
     let children = children(cx)
         .nodes
         .into_iter()
         .map(|child| view! { cx, <li>{child}</li> })
-        .collect_view(cx);
+        .collect::<Vec<_>>();
 
     view! { cx,
-        <ul>{children}</ul>
+      <h1>
+        <code>"<WrapsChildren/>"</code>
+      </h1>
+      // wrap our wrapped children in a UL
+      <ul>{children}</ul>
     }
 }
 
