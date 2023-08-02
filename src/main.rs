c044e00bbe157ae694e0ab4dc4479641eeace156
gihrig <glen@arkadias.com>
2023-08-02 13:50:13 -0700

    leptos tut(lesson): 4.2c Reactivity - Effects as Zero-Cost-ish Abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,46 @@
-/* 4.2b Reactivity - Autotracking and Dynamic Dependencies */
+/* 4.2c Reactivity - Effects as Zero-Cost-ish Abstraction */
 use leptos::*;
 
-// If you’re familiar with a framework like React, you might notice one
-// key difference. React and similar frameworks typically require you to
-// pass a “dependency array,” an explicit set of variables that determine
-// when the effect should rerun.
+// While they’re not a “zero-cost abstraction” in the most technical
+// sense—they require some additional memory use, exist at runtime,
+// etc.—at a higher level, from the perspective of whatever expensive
+// API calls or other work you’re doing within them, effects are a
+// zero-cost abstraction. They rerun the absolute minimum number of
+// times necessary, given how you’ve described them.
 
-// Because Leptos comes from the tradition of synchronous reactive
-// programming, we don’t need this explicit dependency list. Instead, we
-// automatically track dependencies depending on which signals are accessed
-// within the effect.
+// Imagine that I’m creating some kind of chat software, and I want
+// people to be able to display their full name, or just their first
+// name, and to notify the server whenever their name changes:
 
-// This has two effects (no pun intended). Dependencies are:
+/*
+  let (first, set_first) = create_signal(cx, String::new());
+  let (last, set_last) = create_signal(cx, String::new());
+  let (use_last, set_use_last) = create_signal(cx, true);
 
-// 1. Automatic: You don’t need to maintain a dependency list, or worry about
-//    what should or shouldn’t be included. The framework simply tracks
-//    which signals might cause the effect to rerun, and handles it for you.
-// 2. Dynamic: The dependency list is cleared and updated every time the
-//    effect runs. If your effect contains a conditional (for example),
-//    only signals that are used in the current branch are tracked. This
-//    means that effects rerun the absolute minimum number of times.
+  // this will add the name to the log
+  // any time one of the source signals changes
+  create_effect(cx, move |_| {
+      log(
+          cx,
+          if use_last() {
+              format!("{} {}", first(), last())
+          } else {
+              first()
+          },
+      )
+  });
+*/
 
-// If this sounds like magic, and if you want a deep dive into how
-// automatic dependency tracking works, check out this video.
-// (Apologies for the low volume!)
+// If use_last is true, effect should rerun whenever first, last, or
+// use_last changes. But if I toggle use_last to false, a change in last
+// will never cause the full name to change. In fact, last will be
+// removed from the dependency list until use_last toggles again. This
+// saves us from sending multiple unnecessary requests to the API if I
+// change last multiple times while use_last is still false.
 
 fn main() {
     leptos::mount_to_body(|cx| {
         view! { cx,
-        <h1>{"Reactivity - Autotracking and Dynamic Dependencies"}</h1>}
+        <h1>{"Reactivity - Effects as Zero-Cost-ish Abstraction"}</h1>}
     })
 }
