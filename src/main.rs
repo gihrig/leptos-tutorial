3d2766ba1bf48b49ca917d8f291f592f9272bc5b
gihrig <glen@arkadias.com>
2023-08-02 14:02:13 -0700

    leptos tut(lesson): 4.2d Reactivity - To create_effect, or not to create_effect?

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,25 @@
-/* 4.2c Reactivity - Effects as Zero-Cost-ish Abstraction */
+/* 4.2d Reactivity - To create_effect, or not to create_effect? */
 use leptos::*;
 
-// While they’re not a “zero-cost abstraction” in the most technical
-// sense—they require some additional memory use, exist at runtime,
-// etc.—at a higher level, from the perspective of whatever expensive
-// API calls or other work you’re doing within them, effects are a
-// zero-cost abstraction. They rerun the absolute minimum number of
-// times necessary, given how you’ve described them.
+// Effects are intended to run side-effects of the system, not to
+// synchronize state within the system. In other words:
 
-// Imagine that I’m creating some kind of chat software, and I want
-// people to be able to display their full name, or just their first
-// name, and to notify the server whenever their name changes:
+// Don’t write to signals within effects.
 
-/*
-  let (first, set_first) = create_signal(cx, String::new());
-  let (last, set_last) = create_signal(cx, String::new());
-  let (use_last, set_use_last) = create_signal(cx, true);
+// If you need to define a signal that depends on the value of other
+// signals, use a derived signal or create_memo.
 
-  // this will add the name to the log
-  // any time one of the source signals changes
-  create_effect(cx, move |_| {
-      log(
-          cx,
-          if use_last() {
-              format!("{} {}", first(), last())
-          } else {
-              first()
-          },
-      )
-  });
-*/
+// If you need to synchronize some reactive value with the non-reactive
+// world outside—like a web API, the console, the filesystem, or the
+// DOM—create an effect.
 
-// If use_last is true, effect should rerun whenever first, last, or
-// use_last changes. But if I toggle use_last to false, a change in last
-// will never cause the full name to change. In fact, last will be
-// removed from the dependency list until use_last toggles again. This
-// saves us from sending multiple unnecessary requests to the API if I
-// change last multiple times while use_last is still false.
+// If you’re curious for more information about when you should and
+// shouldn’t use create_effect, check out this video for a more in-depth
+// consideration! https://youtu.be/aQOFJQ2JkvQ
 
 fn main() {
     leptos::mount_to_body(|cx| {
         view! { cx,
-        <h1>{"Reactivity - Effects as Zero-Cost-ish Abstraction"}</h1>}
+        <h1>{"Reactivity - To create_effect, or not to create_effect?"}</h1>}
     })
 }
