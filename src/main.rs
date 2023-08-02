0b0b0c8504dc8c38ef56a1a06e4645b653e736f5
gihrig <glen@arkadias.com>
2023-08-02 14:17:28 -0700

    leptos tut(lesson): 4.2e Reactivity - Effects and Rendering

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
-/* 4.2d Reactivity - To create_effect, or not to create_effect? */
+/* 4.2e Reactivity - Effects and Rendering */
 use leptos::*;
 
-// Effects are intended to run side-effects of the system, not to
-// synchronize state within the system. In other words:
+// We’ve managed to get this far without mentioning effects because
+// they’re built into the Leptos DOM renderer. We’ve seen that you can
+// create a signal and pass it into the view macro, and it will update
+// the relevant DOM node whenever the signal changes:
 
-// Don’t write to signals within effects.
+/*
+  let (count, set_count) = create_signal(cx, 0);
 
-// If you need to define a signal that depends on the value of other
-// signals, use a derived signal or create_memo.
+  view! { cx,
+        <p>{count}</p>
+    }
+*/
 
-// If you need to synchronize some reactive value with the non-reactive
-// world outside—like a web API, the console, the filesystem, or the
-// DOM—create an effect.
+// This works because the framework essentially creates an effect
+// wrapping this update. You can imagine Leptos translating this view
+// into something like this:
 
-// If you’re curious for more information about when you should and
-// shouldn’t use create_effect, check out this video for a more in-depth
-// consideration! https://youtu.be/aQOFJQ2JkvQ
+/*
+  let (count, set_count) = create_signal(cx, 0);
+
+  // create a DOM element
+  let p = create_element("p");
+
+  // create an effect to reactively update the text
+  create_effect(cx, move |prev_value| {
+    // first, access the signal’s value and convert it to a string
+    let text = count().to_string();
+
+    // if this is different from the previous value, update the node
+    if prev_value != Some(text) {
+      p.set_text_content(&text);
+    }
+
+    // return this value so we can memoize the next update
+    text
+  });
+*/
+
+// Every time count is updated, this effect wil rerun. This is what
+// allows reactive, fine-grained updates to the DOM.
 
 fn main() {
     leptos::mount_to_body(|cx| {
         view! { cx,
-        <h1>{"Reactivity - To create_effect, or not to create_effect?"}</h1>}
+        <h1>{"Reactivity - Effects and Rendering"}</h1>}
     })
 }
