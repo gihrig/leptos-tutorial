8cd092da75e3a029a3afec0a5959198e0d48cc71
gihrig <glen@arkadias.com>
2023-08-06 22:55:14 -0700

    leptos tut(lesson): 6.2 Async - Suspense

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-/* 6.1 Async - Loading Data with Resources */
+/* 6.2 Async - Suspense */
 
 // Working with async
 
@@ -20,155 +20,138 @@
 
 // ------------------------------------------------------------------
 
-// Loading Data with Resources
+// <Suspense>
 
-// A Resource is a reactive data structure that reflects the current
-// state of an asynchronous task, allowing you to integrate asynchronous
-// Futures into the synchronous reactive system. Rather than waiting
-// for its data to load with .await, you transform the Future into a
-// signal that returns Some(T) if it has resolved, and None if it’s
-// still pending.
-// See: https://docs.rs/leptos/latest/leptos/struct.Resource.html
+// In the previous chapter, we showed how you can create a simple loading
+// screen to show some fallback while a resource is loading.
 
-// You do this by using the `create_resource` function. This takes two
-// arguments (other than the ubiquitous cx):
-
-// 1. a source signal, which will generate a new Future whenever it changes
-// 2. a fetcher function, which takes the data from that signal and
-//    returns a Future
-
-// Here’s an example
 /*
-  // our source signal: some synchronous, local state
   let (count, set_count) = create_signal(cx, 0);
+  let a = create_resource(cx, count, |count| async move { load_a(count).await });
 
-  // our resource
-  let async_data = create_resource(cx,
-    count,
-    // every time `count` changes, this will run
-    |value| async move {
-      log!("loading data from API");
-      load_data(value).await
-    },
-  );
-*/
-
-// To create a resource that simply runs once, you can pass a
-// non-reactive, empty source signal:
-/*
-  let once = create_resource(cx, || (), |_| async move {
-    load_data().await
-  });
-*/
-
-// To access the value you can use `.read(cx)` or
-// `.with(cx, |data| /* */)`.
-// These work just like `.get()` and `.with()` on a signal. `read` clones
-// the value and returns it, `with` applies a closure to it.
-// But there are two differences:
-
-// 1. For any `Resource<_, T>`, they always return `Option<T>`, not `T:`
-//    because it’s always possible that your resource is still loading.
-// 2. They take a Scope argument. You’ll see why in the next chapter, on
-//    `<Suspense/>`.
-
-// So, you can show the current state of a resource in your view:
-/*
-  let once = create_resource(cx, || (), |_| async move {
-    load_data().await
-  });
   view! { cx,
-      <h1>"My Data"</h1>
-      {move || match once.read(cx) {
-          None => view! { cx, <p>"Loading..."</p> }.into_view(cx),
-          Some(data) => view! { cx, <ShowData data/> }.into_view(cx)
-      }}
+    <h1>"My Data"</h1>
+    {move || match once.read(cx) {
+      None => view! { cx, <p>"Loading..."</p> }.into_view(cx),
+      Some(data) => view! { cx, <ShowData data/> }.into_view(cx)
+    }}
   }
 */
 
-// Resources also provide a `refetch()` method that allows you to
-// manually reload the data (for example, in response to a button
-// click) and a `loading()` method that returns a `ReadSignal<bool>`
-// indicating whether the resource is currently loading or not.
+// But what if we have two resources, and want to wait for both of them?
 
-// -----------------------------------------------------------------
-// 6.1 Async - Loading Data with Resources - Example
-// -----------------------------------------------------------------
+/*
+  let (count, set_count) = create_signal(cx, 0);
+  let (count2, set_count2) = create_signal(cx, 0);
+  let a = create_resource(cx, count, |count| async move { load_a(count).await });
+  let b = create_resource(cx, count2, |count| async move { load_b(count).await });
+
+  view! { cx,
+    <h1>"My Data"</h1>
+    {move || match (a.read(cx), b.read(cx)) {
+      (Some(a), Some(b)) => view! { cx,
+        <ShowA a/>
+        <ShowA b/>
+      }.into_view(cx),
+      _ => view! { cx, <p>"Loading..."</p> }.into_view(cx)
+    }}
+  }
+*/
+
+// That’s not so bad, but it’s kind of annoying. What if we could invert
+// the flow of control?
+
+// The <Suspense/> component lets us do exactly that. You give it a
+// fallback prop and children, one or more of which usually involves
+// reading from a resource. Reading from a resource “under” a <Suspense/>
+// (i.e., in one of its children) registers that resource with the
+// <Suspense/>. If it’s still waiting for resources to load, it shows
+// the fallback. When they’ve all loaded, it shows the children.
+
+/*
+  let (count, set_count) = create_signal(cx, 0);
+  let (count2, set_count2) = create_signal(cx, 0);
+  let a = create_resource(cx, count, |count| async move { load_a(count).await });
+  let b = create_resource(cx, count2, |count| async move { load_b(count).await });
+
+  view! { cx,
+    <h1>"My Data"</h1>
+    <Suspense
+    fallback=move || view! { cx, <p>"Loading..."</p> }
+    >
+    <h2>"My Data"</h2>
+    <h3>"A"</h3>
+    {move || {
+      a.read(cx)
+      .map(|a| view! { cx, <ShowA a/> })
+    }}
+    <h3>"B"</h3>
+    {move || {
+      b.read(cx)
+      .map(|b| view! { cx, <ShowB b/> })
+    }}
+    </Suspense>
+  }
+*/
+
+// Every time one of the resources is reloading, the "Loading..."
+// fallback will show again.
+
+// This inversion of the flow of control makes it easier to add or
+// remove individual resources, as you don’t need to handle the matching
+// yourself. It also unlocks some massive performance improvements
+// during server-side rendering, which we’ll talk about during a later
+// chapter.
+
+// ----------------------------------------------------------------
+// 6.2 Async - Suspense - Example
+// ----------------------------------------------------------------
+
 use gloo_timers::future::TimeoutFuture;
 use leptos::*;
 
-// Here we define an async function
-// This could be anything: a network request, database read, etc.
-// Here, we just multiply a number by 10
-async fn load_data(value: i32) -> i32 {
-    // fake a one-second delay
-    TimeoutFuture::new(1_000).await;
-    value * 10
+async fn ucase_api_call(name: String, delay: u32) -> String {
+    TimeoutFuture::new(delay).await;
+    name.to_ascii_uppercase()
+}
+
+async fn lcase_api_call(name: String, delay: u32) -> String {
+    TimeoutFuture::new(delay).await;
+    name.to_ascii_lowercase()
 }
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    // this count is our synchronous, local state
-    let (count, set_count) = create_signal(cx, 0);
+    let (name, set_name) = create_signal(cx, "Bill".to_string());
 
-    // create_resource takes two arguments after its scope
-    let async_data = create_resource(
-        cx,
-        // the first is the "source signal"
-        count,
-        // the second is the loader
-        // it takes the source signal's value as its argument
-        // and does some async work
-        |value| async move { load_data(value).await },
-    );
-    // whenever the source signal changes, the loader reloads
-
-    // you can also create resources that only load once
-    // just return the unit type () from the source signal
-    // that doesn't depend on anything: we just load it once
-    let stable =
-        create_resource(cx, || (), |_| async move { load_data(1).await });
-
-    // we can access the resource values with .read()
-    // this will reactively return None before the Future has resolved
-    // and update to Some(T) when it has resolved
-    let async_result = move || {
-        async_data
-            .read(cx)
-            .map(|value| format!("Server returned {value:?}"))
-            // This loading state will only show before the first load
-            .unwrap_or_else(|| "Loading...".into())
-    };
-
-    // the resource's loading() method gives us a
-    // signal to indicate whether it's currently loading
-    let loading = async_data.loading();
-    let is_loading = move || if loading() { "Loading..." } else { "Idle." };
+    // this will reload every time `name` changes
+    let async_udata = create_resource(cx, name, |name| async move {
+        ucase_api_call(name, 1000).await
+    });
+    let async_ldata = create_resource(cx, name, |name| async move {
+        lcase_api_call(name, 1500).await
+    });
 
     view! { cx,
-        <button on:click=move |_| {
-            set_count.update(|n| *n += 1);
-        }>
+        <input
+            on:input=move |ev| {
+                set_name(event_target_value(&ev));
+            }
 
-            "Click me"
-        </button>
+            prop:value=name
+        />
         <p>
-            <code>"stable"</code>
-            ": "
-            {move || stable.read(cx)}
+            <code>"name:"</code>
+            {name}
         </p>
-        <p>
-            <code>"count"</code>
-            ": "
-            {count}
-        </p>
-        <p>
-            <code>"async_value"</code>
-            ": "
-            {async_result}
-            <br/>
-            {is_loading}
-        </p>
+        // the fallback will show whenever a resource
+        // read "under" the suspense is loading
+        <Suspense fallback=move || view! { cx, <p>"Loading..."</p> }>
+            // the children will be rendered once initially,
+            // and then whenever all resources have been resolved
+            <p>"Your shouting name is " {move || async_udata.read(cx)}</p>
+            <p>"Your quiet name is " {move || async_ldata.read(cx)}</p>
+        </Suspense>
     }
 }
 
