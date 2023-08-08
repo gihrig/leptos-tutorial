8f9d7538d9ccb67e90336bd5eb4553bb382bb7b8
gihrig <glen@arkadias.com>
2023-08-08 16:57:45 -0700

    leptos tut(lesson): 6.4 Async - Actions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-/* 6.3 Async - <Transition> */
+/* 6.4 Async - Actions */
 
 // Working with async
 
@@ -20,94 +20,201 @@
 
 // ------------------------------------------------------------------
 
-// <Transition>
+// Mutating Data with Actions
 
-// You’ll notice in the <Suspense/> example that if you keep reloading
-// the data, it keeps flickering back to "Loading...". Sometimes this
-// is fine. For other times, there’s <Transition/>.
+// We’ve talked about how to load async data with resources. Resources
+// immediately load data and work closely with <Suspense/> and
+// <Transition/> components to show whether data is loading in your app.
+// But what if you just want to call some arbitrary async function and
+// keep track of what it’s doing?
 
-// <Transition/> behaves exactly the same as <Suspense/>, but instead
-// of falling back every time, it only shows the fallback the first
-// time. On all subsequent loads, it continues showing the old data
-// until the new data are ready. This can be really handy to prevent
-// the flickering effect, and to allow users to continue interacting
-// with your application.
+// Well, you could always use spawn_local.
+// https://docs.rs/leptos/latest/leptos/fn.spawn_local.html
+// This allows you to just spawn an async task in a synchronous
+// environment by handing the Future off to the browser (or, on the
+// server, Tokio or whatever other runtime you’re using). But how do
+// you know if it’s still pending? Well, you could just set a signal
+// to show whether it’s loading, and another one to show the result...
 
-// This example shows how you can create a simple tabbed contact list
-// with <Transition/>. When you select a new tab, it continues showing
-// the current contact until the new data loads. This can be a much
-// better user experience than constantly falling back to a loading
-// message.
+// All of this is true. Or you could use the final async primitive:
+// create_action.
+// https://docs.rs/leptos/latest/leptos/fn.create_action.html
+
+// Actions and resources seem similar, but they represent fundamentally
+// different things. If you’re trying to load data by running an async
+// function, either once or when some other value changes, you probably
+// want to use `create_resource`. If you’re trying to occasionally run
+// an async function in response to something like a user clicking a
+// button, you probably want to use `create_action`.
+
+// Say we have some async function we want to run.
+
+/*
+  async fn add_todo_request(new_title: &str) -> Uuid {
+      /* do some stuff on the server to add a new todo */
+  }
+*/
+
+// create_action takes a reactive Scope and an async function that
+// takes a reference to a single argument, which you could think of
+// as its “input type.”
+
+// The input is always a single type. If you want to pass in multiple
+// arguments, you can do it with a struct or tuple.
+
+/*
+  // if there's a single argument, just use that
+  let action1 = create_action(cx, |input: &String| {
+    let input = input.clone();
+    async move { todo!() }
+  });
+
+  // if there are no arguments, use the unit type `()`
+  let action2 = create_action(cx, |input: &()| async { todo!() });
+
+  // if there are multiple arguments, use a tuple
+  let action3 = create_action(cx,
+    |input: &(usize, String)| async { todo!() }
+  );
+*/
+
+// Because the action function takes a reference but the Future needs
+// to have a 'static lifetime, you’ll usually need to clone the value
+// to pass it into the Future. This is admittedly awkward but it
+// unlocks some powerful features like optimistic UI. We’ll see a
+// little more about that in future chapters.
+
+// So in this case, all we need to do to create an action is
+
+/*
+  let add_todo_action = create_action(cx, |input: &String| {
+    let input = input.to_owned();
+    async move { add_todo_request(&input).await }
+  });
+*/
+
+// Rather than calling add_todo_action directly, we’ll call it with
+// .dispatch(), as in
+
+/*
+  add_todo_action.dispatch("Some value".to_string());
+*/
+
+// You can do this from an event listener, a timeout, or anywhere;
+// because .dispatch() isn’t an async function, it can be called from
+// a synchronous context.
+
+// Actions provide access to a few signals that synchronize between
+// the asynchronous action you’re calling and the synchronous reactive
+// system:
+
+/*
+  let submitted = add_todo_action.input(); // RwSignal<Option<String>>
+  let pending = add_todo_action.pending(); // ReadSignal<bool>
+  let todo_id = add_todo_action.value(); // RwSignal<Option<Uuid>>
+*/
+
+// This makes it easy to track the current state of your request, show
+// a loading indicator, or do “optimistic UI” based on the assumption
+// that the submission will succeed.
+
+/*
+  let input_ref = create_node_ref::<Input>(cx);
+
+  view! { cx,
+      <form
+          on:submit=move |ev| {
+              ev.prevent_default(); // don't reload the page...
+              let input = input_ref.get().expect("input to exist");
+              add_todo_action.dispatch(input.value());
+          }
+      >
+          <label>
+              "What do you need to do?"
+              <input type="text"
+                  node_ref=input_ref
+              />
+          </label>
+          <button type="submit">"Add Todo"</button>
+      </form>
+      // use our loading state
+      <p>{move || pending().then("Loading...")}</p>
+  }
+*/
+
+// Now, there’s a chance this all seems a little over-complicated, or
+// maybe too restricted. I wanted to include actions here, alongside
+// resources, as the missing piece of the puzzle. In a real Leptos app,
+// you’ll actually most often use actions alongside server functions,
+// create_server_action,
+// https://docs.rs/leptos/latest/leptos/fn.create_server_action.html
+// and the <ActionForm/> component
+// https://docs.rs/leptos_router/latest/leptos_router/fn.ActionForm.html
+// to create really powerful progressively-enhanced forms.
+// So if this primitive seems useless to you... Don’t worry! Maybe it
+// will make sense later.
+// (Or check out our todo_app_sqlite example now.)
+// https://github.com/leptos-rs/leptos/blob/main/examples/todo_app_sqlite/src/todo.rs
 
 // --------------------------------------------------------------------
-// 6.3 Async - <Transition> - Final Example
+// 6.4 Async - Actions - Final Example
 // --------------------------------------------------------------------
 
 use gloo_timers::future::TimeoutFuture;
-use leptos::*;
+use leptos::{html::Input, *};
+use uuid::Uuid;
 
-async fn important_api_call(id: usize) -> String {
+// Here we define an async function
+// This could be anything: a network request, database read, etc.
+// Think of it as a mutation: some imperative async action you run,
+// whereas a resource would be some async data you load
+async fn add_todo(text: &str) -> Uuid {
+    _ = text;
+    // fake a one-second delay
     TimeoutFuture::new(1_000).await;
-    match id {
-        0 => "Alice",
-        1 => "Bob",
-        2 => "Carol",
-        _ => "User not found",
-    }
-    .to_string()
+    // pretend this is a post ID or something
+    Uuid::new_v4()
 }
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    let (tab, set_tab) = create_signal(cx, 0);
+    // an action takes an async function with a single argument
+    // it can be a simple type, a struct, or ()
+    let add_todo = create_action(cx, |input: &String| {
+        // the input is a reference, but we need the Future to own it
+        // this is important: we need to clone and move into the Future
+        // so it has a 'static lifetime
+        let input = input.to_owned();
+        async move { add_todo(&input).await }
+    });
 
-    // this will reload every time `tab` changes
-    let user_data =
-        create_resource(
-            cx,
-            tab,
-            |tab| async move { important_api_call(tab).await },
-        );
+    // actions provide a bunch of synchronous, reactive variables
+    // that tell us different things about the state of the action
+    let submitted = add_todo.input();
+    let pending = add_todo.pending();
+    let todo_id = add_todo.value();
+
+    let input_ref = create_node_ref::<Input>(cx);
 
     view! { cx,
-        <div class="buttons">
-            <button
-                on:click=move |_| set_tab(0)
-                class:selected=move || tab() == 0
-            >
-                "Tab A"
-            </button>
-            <button
-                on:click=move |_| set_tab(1)
-                class:selected=move || tab() == 1
-            >
-                "Tab B"
-            </button>
-            <button
-                on:click=move |_| set_tab(2)
-                class:selected=move || tab() == 2
-            >
-                "Tab C"
-            </button>
-            // Shows "Loading..." on page load and transition
-            // Seems to defeat the stated purpose of <Transition/>
-            // {move || if user_data.loading().get() { "Loading..." } else { "" }}
-        </div>
-        // Transition seems to be broken in this example.
-        // It works as expected in `hackernews` and `hackernews_axum`
-        // examples
+        <form on:submit=move |ev| {
+            ev.prevent_default();
+            let input = input_ref.get().expect("input to exist");
+            add_todo.dispatch(input.value());
+        }>
 
-        // The <Transition fallback never shows ???
-        // Does not perform as discussed above. It does not "show
-        // initially".
-        // The second feature "subsequent reloads, the current child will
-        // continue showing" does function as expected.
-        // fallback seems to be useless. Might as well be disabled.
-        // `fallback=move || {}`
-        <Transition
-        fallback=move || {view! { cx, <p>"Loading..."</p> }}>
-            <p>{move || user_data.read(cx)}</p>
-        </Transition>
+            <label>
+                "What do you need to do?"
+                <input type="text" node_ref=input_ref/>
+            </label>
+            <button type="submit">"Add Todo"</button>
+        </form>
+        <p style="height: 10px;">{move || pending().then(|| "Loading...")}</p>
+        <p>
+            "Submitted: " <code>{move || format!("{:#?}", submitted())}</code>
+        </p>
+        <p>"Pending: " <code>{move || format!("{:#?}", pending())}</code></p>
+        <p>"Todo ID: " <code>{move || format!("{:#?}", todo_id())}</code></p>
     }
 }
 
