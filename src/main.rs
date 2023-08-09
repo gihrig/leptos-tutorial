d157f286cd699c2088210d1244ea8a01e59399bd
gihrig <glen@arkadias.com>
2023-08-09 14:30:19 -0700

    leptos tut(lesson): 8.0.d Global State Management - Final Example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-/* 8.0.c Global State Management - Create a Global State Struct and Slices */
+/* 8.0.d Global State Management - Final Example */
 
 // So far, we've only been working with local state in components,
 // and we’ve seen how to coordinate state between parent and child
@@ -21,114 +21,204 @@
 //    `create_slice`
 
 // --------------------------------------------------------------------
-// 8.0.c Global State Management - Create a Global State Struct and Slices
+// 8.0.d Global State Management - Final Example
 // --------------------------------------------------------------------
-
-// You may find it cumbersome to wrap each field of a structure in a
-// separate signal like this. In some cases, it can be useful to create
-// a plain struct with non-reactive fields, and then wrap that in a
-// signal.
-
-/*
-  #[derive(Copy, Clone, Debug, Default)]
-  struct GlobalState {
-      count: i32,
-      name: String
-  }
-
-  #[component]
-  fn App(cx: Scope) -> impl IntoView {
-      provide_context(cx, create_rw_signal(GlobalState::default()));
-
-      // etc.
-  }
-*/
-
-// But there’s a problem: because our whole state is wrapped in one
-// signal, updating the value of one field will cause reactive updates
-// in parts of the UI that only depend on the other.
-
-/*
-  let state = expect_context::<RwSignal<GlobalState>>(cx);
-  view! { cx,
-      <button on:click=move |_| state.update(|n| *n += 1)>"+1"</button>
-      <p>{move || state.with(|state| state.name.clone())}</p>
-  }
-*/
-
-// In this example, clicking the button will cause the text inside <p> to be updated, cloning state.name again! Because signals are the atomic unit of reactivity, updating any field of the signal triggers updates to everything that depends on the signal.
-
-// There’s a better way. You can take fine-grained, reactive slices by
-// using `create_memo`
-// https://docs.rs/leptos/latest/leptos/fn.create_memo.html
-// or `create_slice`
-// https://docs.rs/leptos/latest/leptos/fn.create_slice.html
-// (which uses create_memo but also provides a setter).
-// “Memoizing” a value means creating a new reactive value which will
-// only update when it changes. “Memoizing a slice” means creating a
-// new reactive value which will only update when some field of the
-// state struct updates.
-
-// Here, instead of reading from the state signal directly, we create
-// “slices” of that state with fine-grained updates via create_slice.
-// Each slice signal only updates when the particular piece of the
-// larger struct it accesses updates. This means you can create a
-// single root signal, and then take independent, fine-grained slices
-// of it in different components, each of which can update without
-// notifying the others of changes.
-
-/*
-  /// A component that updates the count in the global state.
-  #[component]
-  fn GlobalStateCounter(cx: Scope) -> impl IntoView {
-      let state = expect_context::<RwSignal<GlobalState>>(cx);
-
-      // `create_slice` lets us create a "lens" into the data
-      let (count, set_count) = create_slice(
-          cx,
-          // we take a slice *from* `state`
-          state,
-          // our getter returns a "slice" of the data
-          |state| state.count,
-          // our setter describes how to mutate that slice, given
-          // a new value
-          |state, n| state.count = n,
-      );
-
-      view! { cx,
-          <div class="consumer blue">
-              <button
-                  on:click=move |_| {
-                      set_count(count() + 1);
-                  }
-              >
-                  "Increment Global Count"
-              </button>
-              <br/>
-              <span>"Count is: " {count}</span>
-          </div>
-      }
-  }
-*/
-
-// Clicking this button only updates state.count, so if we create
-// another slice somewhere else that only takes state.name, clicking
-// the button won’t cause that other slice to update. This allows you
-// to combine the benefits of a top-down data flow and of fine-grained
-// reactive updates.
-
-// Note: There are some significant drawbacks to this approach. Both
-// signals and memos need to own their values, so a memo will need to
-// clone the field’s value on every change. The most natural way to
-// manage state in a framework like Leptos is always to provide signals
-// that are as locally-scoped and fine-grained as they can be, not to
-// hoist everything up into global state. But when you do need some
-// kind of global state, `create_slice` can be a useful tool.
 
 use leptos::*;
 
+// In virtual DOM libraries like React, using the Context API to manage
+// global state is a bad idea: because the entire app exists in a tree,
+// changing some value provided high up in the tree can cause the whole
+// app to render.
+//
+// In fine-grained reactive libraries like Leptos, this is simply not
+// the case. You can create a signal in the root of your app and pass
+// it down to other components using provide_context(). Changing it
+// will only cause rerendering in the specific places it is actually
+// used, not the whole app.
+
+#[component]
+fn Option2(cx: Scope) -> impl IntoView {
+    // here we create a signal in the root that can be consumed
+    // anywhere in the app.
+    let (count, set_count) = create_signal(cx, 0);
+    // we'll pass the setter to specific components,
+    // but provide the count itself to the whole app via context
+    provide_context(cx, count);
+
+    view! { cx,
+        <h1>"Option 2: Passing Signals"</h1>
+        // SetterButton is allowed to modify the count
+        <SetterButton set_count/>
+        // These consumers can only read from it
+        // But we could give them write access by passing `set_count`
+        // if we wanted
+        <div style="display: flex">
+            <FancyMath/>
+            <ListItems/>
+        </div>
+    }
+}
+
+/// A button that increments our global counter.
+#[component]
+fn SetterButton(cx: Scope, set_count: WriteSignal<u32>) -> impl IntoView {
+    view! { cx,
+        <div class="provider red">
+            <button on:click=move |_| {
+                set_count.update(|count| *count += 1)
+            }>"Increment Global Count"</button>
+        </div>
+    }
+}
+
+/// A component that does some "fancy" math with the global count
+#[component]
+fn FancyMath(cx: Scope) -> impl IntoView {
+    // here we consume the global count signal with `use_context`
+    let count = use_context::<ReadSignal<u32>>(cx)
+        // we know we just provided this in the parent component
+        .expect("there to be a `count` signal provided");
+    let is_even = move || count() & 1 == 0;
+
+    view! { cx,
+        <div class="consumer blue">
+            "The number " <strong>{count}</strong>
+            {move || if is_even() { " is" } else { " is not" }} " even."
+        </div>
+    }
+}
+
+/// A component that shows a list of items generated from the global count.
+#[component]
+fn ListItems(cx: Scope) -> impl IntoView {
+    // again, consume the global count signal with `use_context`
+    let count = use_context::<ReadSignal<u32>>(cx)
+        .expect("there to be a `count` signal provided");
+
+    let squares = move || {
+        (0..count())
+            .map(|n| view! { cx, <li>{n} <sup>"2"</sup> " is " {n * n}</li> })
+            .collect::<Vec<_>>()
+    };
+
+    view! { cx,
+        <div class="consumer green">
+            <ul>{squares}</ul>
+        </div>
+    }
+}
+
+// Option #3: Create a Global State Struct
+//
+// You can use this approach to build a single global data structure
+// that holds the state for your whole app, and then access it by
+// taking fine-grained slices using `create_slice` or `create_memo`,
+// so that changing one part of the state doesn't cause parts of your
+// app that depend on other parts of the state to change.
+
+#[derive(Default, Clone, Debug)]
+struct GlobalState {
+    count: u32,
+    name: String,
+}
+
+#[component]
+fn Option3(cx: Scope) -> impl IntoView {
+    // We'll provide a single signal that holds the whole state.
+    // Each component will be responsible for creating its own "lens"
+    // into that signal.
+    let state = create_rw_signal(cx, GlobalState::default());
+    provide_context(cx, state);
+
+    view! { cx,
+        <h1>"Option 3: Passing Global State Struct"</h1>
+        <div class="red consumer" style="width: 100%">
+            <h2>"Current Global State"</h2>
+            <pre>{move || { format!("{:#?}", state.get()) }}</pre>
+        </div>
+        <div style="display: flex">
+            <GlobalStateCounter/>
+            <GlobalStateInput/>
+        </div>
+    }
+}
+
+/// A component that updates the count in the global state.
+#[component]
+fn GlobalStateCounter(cx: Scope) -> impl IntoView {
+    let state = use_context::<RwSignal<GlobalState>>(cx)
+        .expect("state to have been provided");
+
+    // `create_slice` lets us create a "lens" into the data
+    let (count, set_count) = create_slice(
+        cx,
+        // we take a slice *from* `state`
+        state,
+        // our getter returns a "slice" of the data
+        |state| state.count,
+        // our setter describes how to mutate that slice, given a new value
+        |state, n| state.count = n,
+    );
+
+    view! { cx,
+        <div class="consumer blue">
+            <button on:click=move |_| {
+                set_count(count() + 1);
+            }>
+
+                "Increment Global Count"
+            </button>
+            <br/>
+            <span>"Count is: " {count}</span>
+        </div>
+    }
+}
+
+/// A component that updates the count in the global state.
+#[component]
+fn GlobalStateInput(cx: Scope) -> impl IntoView {
+    let state = use_context::<RwSignal<GlobalState>>(cx)
+        .expect("state to have been provided");
+
+    // this slice is completely independent of the `count` slice
+    // that we created in the other component
+    // neither of them will cause the other to rerun
+    let (name, set_name) = create_slice(
+        cx,
+        // we take a slice *from* `state`
+        state,
+        // our getter returns a "slice" of the data
+        |state| state.name.clone(),
+        // our setter describes how to mutate that slice, given a new value
+        |state, n| state.name = n,
+    );
+
+    view! { cx,
+        <div class="consumer green">
+            <input
+                type="text"
+                prop:value=name
+                on:input=move |ev| {
+                    set_name(event_target_value(&ev));
+                }
+            />
+
+            <br/>
+            <span>"Name is: " {name}</span>
+        </div>
+    }
+}
+// This `main` function is the entry point into the app
+// It just mounts our component to the <body>
+// Because we defined it as `fn App`, we can now use it in a
+// template as <App/>
+
 fn main() {
     leptos::mount_to_body(|cx| {
-        view! { cx, <h1>"URL as Global State"</h1> }
+        view! { cx,
+            <Option2/>
+            <Option3/>
+        }
     })
 }
