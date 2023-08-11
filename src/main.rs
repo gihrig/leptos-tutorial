8459ecd03f0e9809e8252c58e22b35bedcf77afc
gihrig <glen@arkadias.com>
2023-08-11 13:21:48 -0700

    leptos tut(lesson): 9.4 Routing - The <A/> Component

--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,80 @@
-/* 9.3 Routing - Params and Queries */
+/* 9.4 Routing - The <A/> Component */
 
-// Static paths are useful for distinguishing between different pages,
-// but almost every application wants to pass data through the URL at
-// some point.
+// Client-side navigation works perfectly fine with ordinary HTML <a>
+// elements. The router adds a listener that handles every click on an
+// <a> element and tries to handle it on the client side, i.e., without
+// doing another round trip to the server to request HTML. This is what
+// enables the snappy “single-page app” navigations you’re probably
+// familiar with from most modern web apps.
 
-// There are two ways you can do this:
+// The router will bail out of handling an <a> click under a number of
+// situations
 
-// 1. named route params like id in /users/:id
-// 2. named route queries like q in /search?q=Foo
+// 1. the click event has had prevent_default() called on it
+// 2. the Meta, Alt, Ctrl, or Shift keys were held during click
+// 3. the <a> has a target or download attribute, or rel="external"
+// 4. the link has a different origin from the current location
 
-// Because of the way URLs are built, you can access the query from any
-// <Route/> view. You can access route params from the <Route/> that
-// defines them or any of its nested children.
+// In other words, the router will only try to do a client-side navigation
+// when it’s pretty sure it can handle it, and it will upgrade every <a>
+// element to get this special behavior.
 
-// Accessing params and queries is pretty simple with a couple of hooks:
+// This also means that if you need to opt out of client-side routing,
+// you can do so easily. For example, if you have a link to another page
+// on the same domain, but which isn’t part of your Leptos app, you can
+// just use <a rel="external"> to tell the router it isn’t something it
+// can handle.
 
-// use_query
-// https://docs.rs/leptos_router/latest/leptos_router/fn.use_query.html
-// use_query_map
-// https://docs.rs/leptos_router/latest/leptos_router/fn.use_query_map.html
-// use_params
-// https://docs.rs/leptos_router/latest/leptos_router/fn.use_params.html
-// use_params_map
-// https://docs.rs/leptos_router/latest/leptos_router/fn.use_query_map.html
+// The router also provides an <A> component,
+// https://docs.rs/leptos_router/latest/leptos_router/fn.A.html
+// which does two additional things:
 
-// Each of these comes with a typed option (use_query and use_params) and
-// an untyped option (use_query_map and use_params_map).
+// 1. Correctly resolves relative nested routes. Relative routing with
+//    ordinary <a> tags can be tricky. For example, if you have a route
+//    like /post/:id, <A href="1"> will generate the correct relative
+//    route, but <a href="1"> likely will not (depending on where it
+//    appears in your view.) <A/> resolves routes relative to the path
+//    of the nested route within which it appears.
+// 2. Sets the aria-current attribute to page if this link is the active
+//    link (i.e., it’s a link to the page you’re on). This is helpful for
+//    accessibility and for styling. For example, if you want to set the
+//    link to a different color if it’s a link to the page you’re
+//    currently on, you can match this attribute with a CSS selector.
 
-// The untyped versions hold a simple key-value map. To use the typed
-// versions, derive the Params trait on a struct.
+// Navigating Programmatically
 
-// Params is a very lightweight trait to convert a flat key-value map of
-// strings into a struct by applying `FromStr` to each field. Because of
-// the flat structure of route params and URL queries, it’s significantly
-// less flexible than something like serde; it also adds much less weight
-// to your binary.
+// Your most-used methods of navigating between pages should be with <a>
+// and <form> elements or with the enhanced <A/> and <Form/> components.
+// Using links and forms to navigate is the best solution for
+// accessibility and graceful degradation.
 
-/*
-  use leptos::*;
-  use leptos_router::*;
-
-  #[derive(Params)]
-  struct ContactParams {
-      id: usize
-  }
-
-  #[derive(Params)]
-  struct ContactSearch {
-      q: String
-  }
-*/
-
-// Note: The Params derive macro is located at leptos::Params, and the
-// Params trait is at leptos_router::Params. If you avoid using glob
-// imports like use leptos::*;, make sure you’re importing the right one
-// for the derive macro.
-
-// If you are not using the nightly feature, you will get the error:
-/*
-  no function or associated item named `into_param` found for struct
-  `std::string::String` in the current scope
-*/
-// At the moment, supporting both T: FromStr and Option<T> for typed
-// params requires a nightly feature. You can fix this by simply changing
-// the struct to use q: Option<String> instead of q: String.
-
-// Now we can use them in a component. Imagine a URL that has both params
-// and a query, like /contacts/:id?q=Search.
-
-// The typed versions return Memo<Result<T, _>>. It’s a Memo so it reacts
-// to changes in the URL. It’s a Result because the params or query need
-// to be parsed from the URL, and may or may not be valid.
+// On occasion, though, you’ll want to navigate programmatically, i.e.,
+// call a function that can navigate to a new page. In that case, you
+// should use the `use_navigate` function.
+// https://docs.rs/leptos_router/latest/leptos_router/fn.use_navigate.html
 
 /*
-  let params = use_params::<ContactParams>(cx);
-  let query = use_query::<ContactSearch>(cx);
-
-  // id: || -> usize
-  let id = move || {
-      params.with(|params| {
-          params
-              .map(|params| params.id)
-              .unwrap_or_default()
-      })
-  };
+  let navigate = leptos_router::use_navigate(cx);
+  navigate("/somewhere", Default::default());
 */
 
-// The untyped versions return Memo<ParamsMap>. Again, it’s memo to react
-// to changes in the URL. ParamsMap behaves a lot like any other map
-// type, with a .get() method that returns Option<&String>.
-
+// You should almost never do something like
 /*
-  let params = use_params_map(cx);
-  let query = use_query_map(cx);
-
-  // id: || -> Option<String>
-  let id = move || {
-      params.with(|params| params.get("id").cloned())
-  };
+  <button on:click=move |_| navigate(/* ... */)>.
 */
+// Any on:click that navigates should be an <a>, for reasons of
+// accessibility.
 
-// This can get a little messy: deriving a signal that wraps an Option<_>
-// or Result<_> can involve a couple steps. But it’s worth doing this for
-// two reasons:
+// The second argument here is a set of NavigateOptions,
+// https://docs.rs/leptos_router/latest/leptos_router/struct.NavigateOptions.html
+// which includes options to resolve the navigation relative to the
+// current route as the <A/> component does, replace it in the navigation
+// stack, include some navigation state, and maintain the current scroll
+// state on navigation.
 
-// 1. It’s correct, i.e., it forces you to consider the cases, “What if
-//    the user doesn’t pass a value for this query field? What if they
-//    pass an invalid value?”
-// 2. It’s performant. Specifically, when you navigate between different
-//    paths that match the same <Route/> with only params or the query
-//    changing, you can get fine-grained updates to different parts of
-//    your app without rerendering. For example, navigating between
-//    different contacts in our contact-list example does a targeted
-//    update to the name field (and eventually contact info) without
-//    needing to replace or rerender the wrapping <Contact/>. This is
-//    what fine-grained reactivity is for.
-
-// This is the same example from the previous section. The router is
-// such an integrated system that it makes sense to provide a single
-// example highlighting multiple features, even if we haven’t explained
-// them all yet.
+// Once again, this is the same example. Check out the relative <A/>
+// components, and take a look at the CSS in index.html to see the
+// ARIA-based styling.
 
 // -----------------------------------------------------------------
 // Router Example
