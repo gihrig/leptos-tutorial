cb73fe3917f24f299bfc6c2d38b85201b94d5f47
gihrig <glen@arkadias.com>
2023-08-12 10:28:11 -0700

    leptos tut(lesson): 10.3 Styling - Styled

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-/* 10.2 Styling - Stylers */
+/* 10.3 Styling - Styled */
 
 // Anyone creating a website or application soon runs into the question
 // of styling. For a small app, a single CSS file is probably plenty to
@@ -19,80 +19,99 @@
 // than plain CSS.
 
 // --------------------------------------------------------------------
-// Stylers - Compile-time CSS Extraction
+// Styled: Runtime CSS Scoping
 // --------------------------------------------------------------------
 
-// Stylers
-// https://github.com/abishekatp/stylers
-// is a compile-time scoped CSS library that lets you declare scoped
-// CSS in the body of your component. Stylers will extract this CSS
-// at compile time into CSS files that you can then import into your
-// app, which means that it doesn’t add anything to the WASM binary
-// size of your application.
-
-// This allows you to write components like this:
+// Styled
+// https://github.com/eboody/styled
+// is a runtime scoped CSS library that integrates well with Leptos. It lets you
+// declare scoped CSS in the body of your component function, and then applies those
+// styles at runtime.
 
 /*
-use stylers::style;
+use styled::style;
 
 #[component]
-pub fn App(cx: Scope) -> impl IntoView {
-    let styler_class = style! { "App",
-        two{
-            color: blue;
-        }
-        div.one{
-            color: red;
-            content: raw_str(r#"\hello"#);
-            font: "1.3em/1.2" Arial, Helvetica, sans-serif;
-        }
-        div {
-            border: 1px solid black;
-            margin: 25px 50px 75px 100px;
-            background-color: lightblue;
-        }
-        h2 {
-            color: purple;
-        }
-        @media only screen and (max-width: 1000px) {
-            h3 {
-                background-color: lightblue;
-                color: blue
-            }
-        }
-    };
+pub fn MyComponent(cx: Scope) -> impl IntoView {
+    let styles = style!(
+      div {
+        background-color: red;
+        color: white;
+      }
+    );
 
-    view! { cx, class = styler_class,
-        <div class="one">
-            <h1 id="two">"Hello"</h1>
-            <h2>"World"</h2>
-            <h2>"and"</h2>
-            <h3>"friends!"</h3>
-        </div>
+    styled::view! { cx, styles,
+        <div>"This text should be red with white text."</div>
     }
 }
 */
 
 // ----------------------------------------------------------------
-// Stylers Example from
-// https://github.com/abishekatp/stylers/tree/main/examples/style
+// Styled Example from
+// https://github.com/eboody/styled#usage
 // Seems broken:
-// Compiling stylers v0.3.1
-// error[E0615]: attempted to take value of method `column` on type `proc_macro::Span`
-//   --> /Users/glen/.cargo/registry/src/index.crates.io-6f17d22bba15001f/stylers-0.3.1/src/style/css_style_rule.rs:74:47
-//    |
-// 74 | ...                   pre_col = end.column;
-//    |                                     ^^^^^^ method, not a field
-//    |
-// help: use parentheses to call the method
-//    |
-// 74 |                                 pre_col = end.column();
-// Plus 5 more
+// Compiling leptos-tutorial v0.1.0 (/Users/glen/Documents/Development/Study/Rust/Leptos/leptos-tutorial)
+// error[E0277]: the trait bound `&fn(leptos_reactive::scope::Scope, StyleProps) -> impl leptos_dom::IntoView {Style}: ComponentConstructor<_>` is not satisfied
+//    --> src/main.rs:65:5
+//     |
+// 65  | /     styled::view! {
+// 66  | |         cx,
+// 67  | |         styles,
+// 68  | |         <div>"This text should be red with white text."</div>
+// 69  | |     }
+//     | |_____^ the trait `ComponentConstructor<_>` is not implemented for `&fn(leptos_reactive::scope::Scope, StyleProps) -> impl leptos_dom::IntoView {Style}`
+//     |
+// note: required by a bound in `component_view`
+//    --> /Users/glen/.cargo/registry/src/index.crates.io-6f17d22bba15001f/leptos-0.4.8/src/lib.rs:290:13
+//     |
+// 289 | pub fn component_view<P>(
+//     |        -------------- required by a bound in this function
+// 290 |     f: impl ComponentConstructor<P>,
+//     |             ^^^^^^^^^^^^^^^^^^^^^^^ required by this bound in `component_view`
+//     = note: this error originates in the macro `view` which comes from the expansion of the macro `styled::view` (in Nightly builds, run with -Z macro-backtrace for more info)
 // ----------------------------------------------------------------
 use leptos::*;
-use style_macro::*;
+use styled::style;
+
+#[component]
+pub fn MyComponent(cx: Scope) -> impl IntoView {
+    let styles = style!(
+      div {
+        background-color: red;
+        color: white;
+      }
+    );
+
+    styled::view! {
+        cx,
+        styles,
+        <div>"This text should be red with white text."</div>
+    }
+}
+
+#[component]
+pub fn AnotherComponent(cx: Scope) -> impl IntoView {
+    // note were using a plain div selector and it wont clash with MyComponent's div style!
+    let styles = style!(
+      div {
+        background-color: blue;
+        color: gray;
+      }
+    );
+
+    styled::view! {
+        cx,
+        styles,
+        <div>"This text should be blue with gray text."</div>
+    }
+}
 
 pub fn main() {
     println!["Hello, stylers!"];
-    mount_to_body(|cx| view! { cx,  <Abi/> });
+    mount_to_body(|cx| {
+        view! { cx,
+          <MyComponent/>
+          <AnotherComponent/>
+        }
+    });
 }
