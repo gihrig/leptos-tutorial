027300d26d816e9393274d5e989c844d0b9dc917
gihrig <glen@arkadias.com>
2023-08-28 22:27:30 -0700

    leptos tut(lesson): 12.3.3 Server Side Rendering - In-Order Streaming

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
-/* 12.3.2 Server Side Rendering - Async Rendering */
+/* 12.3.3 Server Side Rendering - In-Order Streaming */
 
-// For example page see https://leptos-rs.github.io/leptos/ssr/23_ssr_modes.html#async-rendering
+// For example page see https://leptos-rs.github.io/leptos/ssr/23_ssr_modes.html#in-order-streaming
 
-// ---------------
-// Async Rendering
-// ---------------
+// ------------------
+// In-Order Streaming
+// ------------------
 
-// Async: load all resources on the server. Wait until all data are loaded, and
-// render HTML in one sweep.
+// In-order streaming: Walk through the component tree, rendering HTML
+// until you hit a <Suspense/>. Send down all the HTML you’ve got so far
+// as a chunk in the stream, wait for all the resources accessed under
+// the <Suspense/> to load, then render it to HTML and keep walking until
+// you hit another <Suspense/> or the end of the page.
 
 // Pros:
-//    Better handling for meta tags (because you know async data even
-//    before you render the <head>). Faster complete load than
-//    synchronous because async resources begin loading on server.
-// Cons:
-//    Slower load time/TTFB: you need to wait for all async resources to
-//    load before displaying anything on the client. The page is totally
-//    blank until everything is loaded.
+//    Rather than a blank screen, shows at least something before the
+//      data are ready.
+// Cons
+//    Loads the shell more slowly than synchronous rendering (or
+//      out-of-order streaming) because it needs to pause at every
+//      <Suspense/>.
+//    Unable to show fallback states for <Suspense/>.
+//    Can’t begin hydration until the entire page has loaded, so earlier
+//      pieces of the page will not be interactive until the suspended
+//      chunks have loaded.
 
 use leptos::*;
 pub fn main() {
     mount_to_body(|cx| {
-        view! { cx, <h1>"Server Side Rendering - Async Rendering"</h1> }
+        view! { cx, <h1>"Server Side Rendering - In-Order Streaming"</h1> }
     });
 }
