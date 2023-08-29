29bffbe9f5e82ba1c3a4d53e3c7a47d0dfc153a9
gihrig <glen@arkadias.com>
2023-08-28 19:03:39 -0700

    leptos tut(lesson): 12.3.1 Server Side Rendering - Synchronous Rendering

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,48 @@
-/* 12.3.0 Server Side Rendering - Async Rendering and SSR “Modes” */
+/* 12.3.1 Server Side Rendering - Synchronous Rendering */
 
-// Server-rendering a page that uses only synchronous data is pretty
-// simple: You just walk down the component tree, rendering each element
-// to an HTML string. But this is a pretty big caveat: it doesn’t answer
-// the question of what we should do with pages that includes
-// asynchronous data, i.e., the sort of stuff that would be rendered
-// under a <Suspense/> node on the client.
+// ---------------------
+// Synchronous Rendering
+// ---------------------
 
-// When a page loads async data that it needs to render, what should
-// we do? Should we wait for all the async data to load, and then render
-// everything at once? (Let’s call this “async” rendering) Should we go
-// all the way in the opposite direction, just sending the HTML we have
-// immediately down to the client and letting the client load the
-// resources and fill them in? (Let’s call this “synchronous” rendering)
-// Or is there some middle-ground solution that somehow beats them both?
-// (Hint: There is.)
+//
+// Synchronous: Serve an HTML shell that includes fallback for any
+// <Suspense/>. Load data on the client using create_local_resource,
+// replacing fallback once resources are loaded.
 
-// If you’ve ever listened to streaming music or watched a video online,
-// I’m sure you realize that HTTP supports streaming, allowing a single
-// connection to send chunks of data one after another without waiting
-// for the full content to load. You may not realize that browsers are
-// also really good at rendering partial HTML pages. Taken together,
-// this means that you can actually enhance your users’ experience by
-// streaming HTML: and this is something that Leptos supports out of the
-// box, with no configuration at all. And there’s actually more than one
-// way to stream HTML: you can stream the chunks of HTML that make up
-// your page in order, like frames of a video, or you can stream them...
-// well, out of order.
+// Pros:
+//  App shell appears very quickly: great TTFB (time to first byte).
+// Cons:
+//  Resources load relatively slowly; you need to wait for JS + WASM to
+//    load before even making a request.
+//  No ability to include data from async resources in the <title> or
+//    other <meta> tags, hurting SEO and things like social media link
+//    previews.
 
-// Let me say a little more about what I mean.
+// If you’re using server-side rendering, the synchronous mode is almost
+// never what you actually want, from a performance perspective. This is
+// because it misses out on an important optimization. If you’re loading
+// async resources during server rendering, you can actually begin
+// loading the data on the server. Rather than waiting for the client to
+// receive the HTML response, then loading its JS + WASM, then realize it
+// needs the resources and begin loading them, server rendering can
+// actually begin loading the resources when the client first makes the
+// response. In this sense, during server rendering an async resource is
+// like a Future that begins loading on the server and resolves on the
+// client. As long as the resources are actually serializable, this will
+// always lead to a faster total load time.
 
-// Leptos supports all four different modes of rendering HTML that
-// includes asynchronous data:
-
-// 1. Synchronous Rendering
-//    https://leptos-rs.github.io/leptos/ssr/23_ssr_modes.html#synchronous-rendering
-// 2. Async Rendering
-//    https://leptos-rs.github.io/leptos/ssr/23_ssr_modes.html#async-rendering
-// 3. In-Order streaming
-//    https://leptos-rs.github.io/leptos/ssr/23_ssr_modes.html#in-order-streaming
-// 4. Out-of-Order Streaming
-//    https://leptos-rs.github.io/leptos/ssr/23_ssr_modes.html#out-of-order-streaming
+// This is why create_resource
+// https://docs.rs/leptos/latest/leptos/fn.create_resource.html
+// requires resources data to be serializable by default, and why you
+// need to explicitly use create_local_resource
+// https://docs.rs/leptos/latest/leptos/fn.create_local_resource.html
+// for any async data that is not serializable and should therefore only
+// be loaded in the browser itself. Creating a local resource when you
+// could create a serializable resource is always a deoptimization.
 
 use leptos::*;
 pub fn main() {
     mount_to_body(|cx| {
-        view! { cx, <h1>"Server Side Rendering - Async Rendering and SSR 'Modes'"</h1> }
+        view! { cx, <h1>"Server Side Rendering - Synchronous Rendering"</h1> }
     });
 }
